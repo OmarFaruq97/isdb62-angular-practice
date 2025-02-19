29eb0b204346a11f0e7173ee5ba731579eddf8d8
Omar Faruq <omarfaruq9723@gmail.com>
2025-02-20 00:47:36 +0600

    initial
    work for edit and permanent delete

--- local_storage_crud/src/app/create-user/create-user.component.ts
+++ local_storage_crud/src/app/create-user/create-user.component.ts
@@ -4,44 +4,43 @@ import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 
-
-
-
-
-
 @Component({
   selector: 'app-create-user',
   imports: [FormsModule],
   templateUrl: './create-user.component.html',
   styleUrl: './create-user.component.css',
 })
-
-export class CreateUserComponent{
-  user:User = new User('', 0,'','');
+export class CreateUserComponent {
+  user: User = new User('', 0, '', '');
   isUpdate = false;
 
-  constructor(private router: Router){
+  constructor(private router: Router) {
     const nav = this.router.getCurrentNavigation();
     if (nav?.extras.state && nav?.extras.state['user']) {
+      this.isUpdate = nav.extras.state['user'];
       this.isUpdate = true;
     }
   }
 
-  // ngOnInit(): void {        
-  // }
-  onSubmit(){
-    let users: User[]= JSON.parse(localStorage.getItem('users') || '[]');
+  ngOnInit(): void {}
 
-    if(this.isUpdate){
-      users = users.map(user => (user.phone == this.user.phone? this.user : user))
-    }else{
-      users.push(this.user)
+  onSubmit() {
+    let users: User[] = JSON.parse(localStorage.getItem('users') || '[]');
+
+    if (this.isUpdate) {
+      users = users.map((user) =>
+        user.phone == this.user.phone ? this.user : user
+      );
+    } else {
+      users.push(this.user);
     }
-    users.push(this.user);
 
-    localStorage.setItem('users',JSON.stringify(users));
+    //  JSON.stringify make to String
 
-    this.user = new User ('',0,'','');   
+    localStorage.setItem('users', JSON.stringify(users));
+
+    this.user = new User('', 0, '', '');
     // alert('New user create successfully');
+    this.router.navigate(['/user-list']);
   }
 }
--- local_storage_crud/src/app/user-list/user-list.component.ts
+++ local_storage_crud/src/app/user-list/user-list.component.ts
@@ -2,39 +2,30 @@ import { Component, OnInit } from '@angular/core';
 import { User } from '../app.component';
 import { Router } from '@angular/router';
 
-import { state } from '@angular/animations';
-
 @Component({
   selector: 'app-user-list',
   imports: [],
   templateUrl: './user-list.component.html',
-  styleUrl: './user-list.component.css'
+  styleUrl: './user-list.component.css',
 })
-
-
 export class UserListComponent implements OnInit {
-Users: User[] = [];
-users: any;
+  Users: User[] = [];
 
-constructor(private router: Router) {  
-}  
 
-ngOnInit(): void {
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
     this.Users = JSON.parse(localStorage.getItem('users') || '[]');
-}
-editUser(user: User){
-  this.router.navigate(['/create-user'], {state:{User}});  
-}
+  }
 
-deleteUser(user: User){
-  if (confirm('Are you sure delete?')) {
-    this.Users = this.Users.filter(u=> u !==user);
-    localStorage.setItem('user',JSON.stringify(this.Users))
+  editUser(User: User) {
+    this.router.navigate(['/create-user'], { state: { User } });
+  }
+
+  deleteUser(User: User) {
+    if (confirm('Are you sure delete?')) {
+      this.Users = this.Users.filter((u) => u !== User);
+      localStorage.setItem('user', JSON.stringify(this.Users));
+    }
   }
 }
-
-}
-
-
-
-
