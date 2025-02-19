14d8be77f36267bc4221293a078b44436196345f
Omar Faruq <omarfaruq9723@gmail.com>
2025-02-19 22:34:41 +0600

    pull

--- local_storage_crud/src/app/app.component.ts
+++ local_storage_crud/src/app/app.component.ts
@@ -1,26 +1,34 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavbarComponent } from "./navbar/navbar.component";
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, NavbarComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'local_storage_crud';
 }
-export class User{
+export class User {
   name: string;
-  age:number;
-  phone:string;
+  age: number;
+  phone: string;
   address: string;
 
+<<<<<<< Updated upstream
   constructor (name: string, age: number, phone: string, address: string){
     this.name=name;
     this.age=age;
     this.phone=phone;
     this.address=address;
+=======
+  constructor(name: string, age: number, phone: string, address: string) {
+    this.name = name;
+    this.age = age;
+    this.phone = phone;
+    this.address = address;
+>>>>>>> Stashed changes
   }
 }
--- local_storage_crud/src/app/user-list/user-list.component.ts
+++ local_storage_crud/src/app/user-list/user-list.component.ts
@@ -1,6 +1,14 @@
+<<<<<<< Updated upstream
 import { User } from './../app.component';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+=======
+import { Component, OnInit } from '@angular/core';
+import { User } from '../app.component';
+import { Router } from '@angular/router';
+import { JsonPipe } from '@angular/common';
+import { state } from '@angular/animations';
+>>>>>>> Stashed changes
 
 @Component({
   selector: 'app-user-list',
@@ -8,6 +16,7 @@ import { Router } from '@angular/router';
   templateUrl: './user-list.component.html',
   styleUrl: './user-list.component.css'
 })
+<<<<<<< Updated upstream
 export class UserListComponent implements OnInit {
 Users: User[] = [];
 
@@ -27,5 +36,25 @@ deleteUser(user: User){
     localStorage.setItem('user',JSON.stringify(this.Users))
   }
 }
+=======
+export class UserListComponent implements OnInit{
+  Users: User[]= [];
+>>>>>>> Stashed changes
 
+  constructor(private router: Router){
+
+  }
+  ngOnInit(): void {
+      this.Users= JSON.parse(localStorage.getItem('users') ||'[]');
+  }
+  editUser(user:User){
+    this.router.navigate(['/create-user'], {state:{user}});
+  }
+  deleteUser(user: User){
+    if (confirm('Are you sure delete?')) {
+      this.Users = this.Users.filter(u=> u !==user);
+      localStorage.setItem('user',JSON.stringify(this.Users))
+    }
+  }
 }
+
