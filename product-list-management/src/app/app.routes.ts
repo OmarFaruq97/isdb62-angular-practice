63638e096f91090e47867d9e38faf2c04b29bb86
Omar Faruq <omarfaruq9723@gmail.com>
2025-02-25 13:53:50 +0600

    initial

--- product-list-management/src/app/app.routes.ts
+++ product-list-management/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { AddProductComponent } from './page/add-product/add-product.component';
 import { ListProductComponent } from './page/list-product/list-product.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
+    { path: '', component: HomeComponent},
     { path: 'add-product', component:AddProductComponent},
     { path: 'list-product', component:ListProductComponent}
 ];
