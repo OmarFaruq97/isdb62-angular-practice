77dc889d3af64f14b7fe2f19c2685ff0463aac1a
Omar Faruq <omarfaruq9723@gmail.com>
2025-03-03 00:19:49 +0600

    web_app_update
    initial

--- web_app_project_01/src/app/app.component.ts
+++ web_app_project_01/src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
-import { TableComponent } from './table/table.component';
+import { HomeComponent } from "./home/home.component";
 import { NavbarComponent } from "./navbar/navbar.component";
+import { AboutUsComponent } from "./about-us/about-us.component";
+
 
 @Component({
   selector: 'app-root',
-  imports: [FooterComponent, HeaderComponent, SidebarComponent, SidebarComponent, TableComponent, NavbarComponent],
+  imports: [FooterComponent, HeaderComponent, NavbarComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
