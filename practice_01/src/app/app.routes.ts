8f81ed99dac0845d38ba10105abcfc280025f086
Omar Faruq <omarfaruq9723@gmail.com>
2025-03-14 22:34:11 +0600

    updated

--- practice_01/src/app/app.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Routes } from '@angular/router';
-import { CountAppComponent } from './count-app/count-app.component';
-
-export const routes: Routes = [    
-    {path: '', redirectTo: ''},
-    { path: 'count-app', component : CountAppComponent}
-];
